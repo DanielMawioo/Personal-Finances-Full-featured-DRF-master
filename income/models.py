f7bc59c7ac4f9ab1d83758c45cdc969103b2e538
ader-api <aderapi6@gmail.com>
2021-11-10 13:23:24 +0300

    update

--- income/models.py
+++ income/models.py
@@ -5,22 +5,22 @@ from authentication.models import User
 # Create your models here.
 
 
-class Expense(models.Model):
+class Income(models.Model):
 
-    CATEGORY_OPTIONS = [
-        ('ONLINE_SERVICES', 'ONLINE_SERVICES'),
-        ('TRAVEL', 'TRAVEL'),
-        ('FOOD', 'FOOD'),
-        ('RENT', 'RENT'),
+    SOURCE_OPTIONS = [
+        ('SALARY', 'SALARY'),
+        ('BUSINESS', 'BUSINESS'),
+        ('SIDE-HUSTLES', 'SIDE-HUSTLES'),
         ('OTHERS', 'OTHERS')
     ]
 
-    category = models.CharField(choices=CATEGORY_OPTIONS, max_length=255)
+    source = models.CharField(choices=SOURCE_OPTIONS, max_length=255)
     amount = models.DecimalField(
         max_digits=10, decimal_places=2, max_length=255)
     description = models.TextField()
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
